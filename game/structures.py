77744575e70cc184087578b4da9fd7ca582433bf
nkumar15-brown-university <43328693+nkumar15-brown-university@users.noreply.github.com>
2020-02-24 00:22:52 -0500

    [#2] added new MarketSegment class and updated all other classes correspondingly

--- game/structures.py
+++ game/structures.py
@@ -1,43 +1,65 @@
 from dataclasses import dataclass
-from typing import Set, List, Dict
+from typing import Set, List, Dict, OrderedDict
+import uuid
 
 from prettytable import PrettyTable
 import game.game as game
 
+
+
+@dataclass
+class MarketSegment:
+    """
+    Represents a market segment
+    """
+    uid: int
+    demographics: Set[str]
+
+    def __init__(self, demographics: Set[str]):
+        self.uid = uuid.uuid4().int
+        self.demographics = demographics
+
+    def __eq__(self, other):
+        return self.demographics == other.demographics
+
+    def __matches__(self, other):
+        return other.demographics.issubset(self.demographics)
 
 @dataclass
 class Good:
     """
     Represents an impression opportunity
     """
-    id: str
-    market_segments: Set[str]
-    supply: int
-    reserve_price: float
+    uid: int
+    #id: str
+    market_segment: MarketSegment
+    supply: int ## TODO: what is this?
+    reserve_price: float ## TODO: what is this?
 
-    def __init__(self, name: Set[str], supply: int, reserve_price: float):
-        self.market_segments = name
+    def __init__(self, market_segment: MarketSegment, supply: int, reserve_price: float):
+        self.uid = uuid.uuid4().int
+        self.market_segment = market_segment
         self.supply = supply
-        self.reserve_price = reserve_price
-        list_of_segments = list(self.market_segments)
-        list_of_segments.sort()
-        self.id = "".join([x[0] for x in list_of_segments])
+        self.reserve_price = reserve_price   
+        #list_of_segments = list(self.market_segments)
+        #list_of_segments.sort()
+        #self.id = "".join([x[0] for x in list_of_segments])
         # self.id = "".join(list_of_segments)
 
     def __repr__(self):
-        return "(" + self.id + "," + str(self.supply) + "," + str(self.reserve_price) + ")"
+        return "(" + self.uid + "," + str(self.supply) + "," + str(self.reserve_price) + ")"
 
     def __hash__(self):
-        return hash(self.id)
+        return hash(self.uid)
 
     def __lt__(self, other):
         return self.supply <= other.supply
 
     def __eq__(self, other):
-        return self.market_segments == other.market_segments
+        return self.market_segment == other.market_segment ## TODO: should equality be by uid or by market segment?
 
     def __matches__(self, other):
-        return other.market_segments.issubset(self.market_segments)
+        return self.market_segment.__matches__(other.market_segment)
 
 
 @dataclass
@@ -45,20 +67,36 @@ class Campaign:
     """
     Represents a campaign
     """
-    name: str
+    uid: int
     reach: int
     budget: float
     spend: float ## How much has been spent so far on the campaign (e.g. via bids won)
-    target: Good
+    remaining_reach: int ## How much of the reach is left
+    target: MarketSegment ## Which market segment to target
+
+    def __init__(self, reach, budget, target):
+        self.uid = uuid.uuid4().int
+        self.reach = reach
+        self.budget = budget
+        self.spend = 0.0
+        self.remaining_reach = reach
+        self.target = target
 
     def __repr__(self):
-        return "(" + self.name + ", " + self.target.id + ", " + str(self.reach) + ", " + str(self.budget) + ", " + str(self.budget / self.reach) + ")"
+        return "(" + str(self.uid) + ", " + str(self.target) + ", " + str(self.reach) + ", " + str(self.budget) + ", " + str(self.budget / self.reach) + ")"
 
     def __lt__(self, other):
         return (self.budget / self.reach) <= (other.budget / other.reach)
 
     def __hash__(self):
-        return hash(self.name)
+        return hash(self.uid)
+
+    def to_vector(campaign: Campaign):
+        return []
+
+    def from_vector(campaign_vector: List, all_possible_market_segments: Set[MarketSegment]):
+        camp = Campaign(-1, -1.0, None)
+        return camp
 
 
 @dataclass
@@ -68,24 +106,27 @@ class Market:
     """
     campaigns: List[Campaign]
     goods: List[Good] ## TODO: rename this to impression_opps
-    campaigns_pmf: Dict[Campaign, float]
-    impression_opps_pmf: Dict[Good, float]
+    campaigns_pmf: OrderedDict[List[T], float]
+    market_segments_pmf: Dict[MarketSegment, float]
 
-    def __init__(self, campaigns_pmf: Dict[Campaign, float], impression_opps_pmf: Dict[Good, float]):
+    def __init__(self, campaigns_pmf: OrderedDict[List[T], float], market_segments_pmf: OrderedDict[MarketSegment, float]):
     	self.campaigns_pmf = campaigns_pmf
-    	self.impression_opps_pmf = impression_opps_pmf
+    	self.market_segments_pmf = market_segments_pmf
 
     def draw_campaigns(self, draw_amount: int, clear_current = True):
     	if clear_current:
     		self.campaigns = []
     	for i in range(draw_amount):
-    		self.campaigns.append(game.draw_one(self.campaigns_pmf))
+            campaign_specification = game.draw_one(self.campaigns_pmf)
+            camp = Campaign(reach, budget, )
+    		self.campaigns.append(camp)
 
     def draw_impression_opps(self, draw_amount: int, clear_current = True):
     	if clear_current:
     		self.goods = []
     	for i in range(draw_amount):
-    		self.goods.append(game.draw_one(self.impression_opps_pmf)) 
+            imp_opp = Good(game.draw_one(self.market_segments_pmf), 1, 0.0)
+    		self.goods.append(imp_opp)
 
     def initialize(self, num_of_campaigns: int, num_of_impression_opps: int):
     	self.draw_campaigns(num_of_campaigns)
@@ -93,7 +134,7 @@ class Market:
     	return self.campaigns, self.goods
 
     def run_auction(self, bids, update_campaigns = True):
-    	allocations, expenditure = game.run_auctions(self.goods, self.impression_opps_pmf.keys(), self.campaigns, bids)
+    	allocations, expenditure = game.run_auctions(self.goods, self.market_segments_pmf.keys(), self.campaigns, bids) ## TODO: this is broken now
     	## allocations = {c: {g: 0 for g in goods} for c in campaigns}
     	## expenditure = {c: {g: 0 for g in goods} for c in campaigns}
     	if (update_campaigns):
@@ -101,7 +142,7 @@ class Market:
     			temp_allocs = allocations[campaign]
     			temp_exp = expenditure[campaign]
     			for imp_count in temp_allocs.values():
-    				campaign.reach -= imp_count
+    				campaign.remaining_reach -= imp_count
     			for cost in temp_exp.values():
     				campaign.spend += cost
     	return allocations, expenditure
@@ -146,27 +187,27 @@ class Allocation:
 @dataclass
 class Bid:
     """
-    A bid of a campaign is for a good and specifics a bid amount and bid limit.
+    A bid of a campaign is for a market segment and specifics a bid amount and bid limit.
     """
     campaign: Campaign
-    good: Good
+    market_segment: MarketSegment
     bid: float
     limit: float
 
-    def __init__(self, campaign: Campaign, good: Good, bid: float, limit: float):
+    def __init__(self, campaign: Campaign, market_segment: MarketSegment, bid: float, limit: float):
         self.campaign = campaign
-        self.good = good
+        self.market_segment = market_segment
         self.bid = bid
         self.limit = limit
         # A bid cannot be negative
         assert self.bid >= 0
         # A limit cannot be non-positive
         assert self.limit > 0
-        # A bid cannot be bigger than its limit, since in the worst case a bidder could en up paying a price arbitrarily close to its bid.
+        # A bid cannot be bigger than its limit, since in the worst case a bidder could end up paying a price arbitrarily close to its bid.
         assert self.limit >= self.bid
 
     def __repr__(self):
-        return str(self.campaign) + " bid " + str(self.bid) + " for " + str(self.good) + " with limit " + str(self.limit)
+        return str(self.campaign) + " bid " + str(self.bid) + " for " + str(self.market_segment) + " with limit " + str(self.limit)
 
     def __lt__(self, other):
         return self.bid <= other.bid
